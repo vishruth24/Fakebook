54627ebacbce4fe3a975d04c19716f2c12b82530
Vishruth <vishruth24@gmail.com>
2019-03-06 00:26:04 +0530

    Updated to 2019

--- googlecalapi.py
+++ googlecalapi.py
@@ -7,7 +7,7 @@ def addtocalapi(birthdays):
     # Setup the Calendar API
     SCOPES = 'https://www.googleapis.com/auth/calendar'
     store = file.Storage('credentials.json')
-    creds = store.get()
+    creds = None
     if not creds or creds.invalid:
         flow = client.flow_from_clientsecrets('client_secret.json', SCOPES)
         creds = tools.run_flow(flow, store)
@@ -18,8 +18,8 @@ def addtocalapi(birthdays):
         day,month=v.split('/')
         EVENT={
         'summary':'It\'s %s\'s birthday today!'% k,
-        'start':{'dateTime':'2018-{}-{}T00:00:00{}'.format(month,day,GMT_OFF)},
-        'end':{'dateTime':'2018-{}-{}T14:00:00{}'.format(month,day,GMT_OFF)},
+        'start':{'dateTime':'2019-{}-{}T00:00:00{}'.format(month,day,GMT_OFF)},
+        'end':{'dateTime':'2019-{}-{}T14:00:00{}'.format(month,day,GMT_OFF)},
         }
 
         e=CAL.events().insert(calendarId='primary',body=EVENT).execute()
